e5acfbaeb6704611cd65c2dbe06260ba6679a7cc
coral-technology <mohamedazzam@coral.io>
2021-04-05 22:45:58 +0200

    new pages

--- js/main.js
+++ js/main.js
@@ -100,22 +100,3 @@ $(window).scroll(function () {
 
 //----------------------------History
 
-
-var timelineSwiper = new Swiper ('.timeline .swiper-container', {
-    direction: 'vertical',
-    loop: false,
-    speed: 1600,
-    pagination: '.swiper-pagination',
-    paginationBulletRender: function (swiper, index, className) {
-      var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
-      return '<span class="' + className + '">' + year + '</span>';
-    },
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    breakpoints: {
-      768: {
-        direction: 'horizontal',
-      }
-    }
-  });
