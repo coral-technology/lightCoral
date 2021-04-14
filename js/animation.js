72f470ba00703d56ace7642000ea86d2cbdcffa4
coral-technology <mohamedazzam@coral.io>
2021-04-14 05:39:13 +0200

    New Platform

--- js/animation.js
+++ js/animation.js
@@ -1,56 +1,6 @@
 
 
 
-
-var $button = document.querySelector(".toggle");
-var $close = document.querySelector(".close");
-// on click event
-$button.addEventListener('click', function () {
-    if ($button.classList.contains("active")) {
-        // if has 'selected' class remove class
-        $('#particles-js').removeClass('active');
-        $('#target').removeClass('active');
-        $('.portal').removeClass('activeUp');
-        $('.container').removeClass('active');
-        $button.classList.remove("active");
-        $close.classList.remove("close");
-    } else {
-        // otherwise add 'selected' class
-        $('#particles-js').addClass('active');
-        $('#target').addClass('active');
-        $('.portal').addClass('activeUp');
-        $('.container').addClass('active');
-        $button.classList.add("active");
-        $close.classList.add("close");
-    }
-});
-
-$close.addEventListener('click', function () {
-    if ($close.classList.contains("close")) {
-        // if has 'selected' class remove class
-        $('#particles-js').removeClass('active');
-        $('#target').removeClass('active');
-        $('.portal').removeClass('activeUp');
-        $('.container').removeClass('active');
-        $close.classList.remove("close");
-        $button.classList.remove("active");
-    } else {
-        // otherwise add 'selected' class
-        $('#particles-js').addClass('close');
-        $('#target').addClass('active');
-        $('.portal').addClass('activeUp');
-        $('.container').addClass('active');
-        $close.classList.add("close");
-        $button.classList.add("active");
-    }
-});
-
-
-
-
-
-
-
 function animateFrom(elem, direction) {
     direction = direction | 1;
     
